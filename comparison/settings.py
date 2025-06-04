d74dd0a760bc34011150f0508dfdd300325b8a47
Aidan Szilagyi <aidanszilagyi@gmail.com>
2025-06-04 01:27:21 -0400

    added heroku url to allowed hosts

--- comparison/settings.py
+++ comparison/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["comparison-site-49452db2da77.herokuapp.com"]
 
 
 # Application definition
