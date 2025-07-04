7503443d621c358af670e3fc08c7028840dcf10e
Aidan Szilagyi <aidanszilagyi@gmail.com>
2025-07-04 16:13:53 -0400

    tried again to fix CI

--- comparison/settings.py
+++ comparison/settings.py
@@ -92,13 +92,24 @@ WSGI_APPLICATION = 'comparison.wsgi.application'
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        default=DATABASE_URL, 
+#        conn_max_age=600, 
+#        ssl_require=True
+#    )
+#}
 DATABASES = {
-    'default': dj_database_url.config(
-        default=DATABASE_URL, 
-        conn_max_age=600, 
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': os.environ.get('DB_NAME'),                     
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),                     
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
